fa7a9c4f9c2517e2074a9914f35bde4d463a0d1c
Kristina Adigamova <kriwowak@yandex.ru>
2022-09-19 19:33:30 +0500

    3 урок ДЗ 1 задание

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println(dog + 4);
+        System.out.println(cat + 4);
+        System.out.println(paper + 4);
+        System.out.println(dog - 3.5);
+        System.out.println(cat - 1.6);
+        System.out.println(paper - 7639);
+
+    }
+}
